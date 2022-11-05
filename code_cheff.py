993269fd383d0fb4aafbd0b3ee1113f80eb0b104
manuprakash <manu.prakash2020@vitbhopal.ac.in>
2022-11-06 00:04:46 +0530

    another que

--- code_cheff.py
+++ code_cheff.py
@@ -3,15 +3,11 @@ for _ in range(int(input())):
     sat = list(map(int, input().split()))
     mot = list(map(int, input().split()))
     q_list = []
-    for i in range(q):
-        temp = int(input())
-        q_list.append(temp)
     sum = []
     for i in sat:
         for j in mot:
             sum.append(i + j)
     sum.sort()
-    print(sum)
-    for i in q_list:
-        print(sum[i - 1])
+    for i in range(q):
+        print(sum[int(input()) - 1] )
 # this new line
