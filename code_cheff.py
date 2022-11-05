64313414cb0b8faa34ab98f7c7a07ba3d2b8f4b5
manuprakash <manu.prakash2020@vitbhopal.ac.in>
2022-11-06 00:14:20 +0530

    ad

--- code_cheff.py
+++ code_cheff.py
@@ -2,12 +2,11 @@ for _ in range(int(input())):
     k, q = map(int, input().split())
     sat = list(map(int, input().split()))
     mot = list(map(int, input().split()))
-    q_list = []
     sum = []
     for i in sat:
         for j in mot:
             sum.append(i + j)
     sum.sort()
     for i in range(q):
-        print(sum[int(input()) - 1] )
+        print(sum[int(input()) - 1])
 # this new line
