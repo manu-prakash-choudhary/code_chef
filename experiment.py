1b24a01a83c1da033f9bcf0a1b336a20cc107863
manuprakash <manu.prakash2020@vitbhopal.ac.in>
2023-01-01 17:18:30 +0530

    as

--- experiment.py
+++ experiment.py
@@ -1,3 +1,6 @@
-a = [1,2,3,4]
-a.append(1)
-print(a)
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+
+df = pd.read_csv(r'C:\Users\choud\Downloads\Final Report Databases\1. GDP\GDP by Country 1999-2022.csv')
+df
