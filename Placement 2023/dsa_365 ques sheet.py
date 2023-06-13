99a7ed6f1b3f3f2cb025692a687049a5946c1dd2
manuprakash <manu.prakash2020@vitbhopal.ac.in>
2023-06-13 09:38:02 +0530

    sd

--- Placement 2023/dsa_365 ques sheet.py
+++ Placement 2023/dsa_365 ques sheet.py
@@ -16,6 +16,7 @@ if que1:
         sum_sub_array = sum(nums[start:end])
         maxSum = max(maxSum, sum_sub_array)
         return sum_subarray_recursive(nums, start, maxSum, end+1)
+
         
     class Solution1:
         # one approach
@@ -67,7 +68,7 @@ if que1:
     ))
 
 # que 4 Contains Duplicate
-que2 = True
+que2 = False
 if que2:
     class Solution2:
         def containsDuplicate(self, nums: List[int]) -> bool:
@@ -80,6 +81,23 @@ if que2:
     # Input: nums = [1,2,3,4]
     solObj = Solution2()
     print(sys._getframe(0).f_lineno, 'Que 2', solObj.containsDuplicate(nums=[1,2,3,4]))
+
+# Que 5 Chocolate Distribution Problem
+que3 = True
+if que3:
+    # Given an array of N integers where each value represents the number of chocolates in a packet. Each packet can have a variable number of chocolates. There are m students, the task is to distribute chocolate packets such that: 
+        # Each student gets one packet.
+        # The difference between the number of chocolates in the packet with maximum chocolates and the packet with minimum chocolates given to the students is minimum.
+
+    # Examples 
+    # Input : arr[] = {7, 3, 2, 4, 9, 12, 56} , m = 3 
+    # Output: Minimum Difference is 2 
+    # Explanation:
+    # We have seven packets of chocolates and we need to pick three packets for 3 students 
+    # If we pick 2, 3 and 4, we get the minimum difference between maximum and minimum packet sizes.
+
+    # Input : arr[] = {3, 4, 1, 9, 56, 7, 9, 12} , m = 5 
+    # Output: Minimum Difference is 6 
 
 
 
